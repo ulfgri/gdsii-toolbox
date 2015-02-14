e631f6378ed5ed92749e04a7ca935d715c8987d2
Ulf GRIESMANN <ulfgri@gmail.com>
2015-02-14 15:46:48 -0500

    14feb2015 - new @gds_library/getstruct.m for copying structures from libraries   by name - changed @gds_library/subtree.m to return a cell array of structures   instead of structure indices.

---
+++ Basic/@gds_library/getstruct.m
@@ -0,0 +1,38 @@
+function cas = getstruct(glib, sname);
+%function cas = getstruct(glib, sname);
+%
+% getstruct: copy structures from libraries by name. The method
+%            returns a cell array with gds_structure 
+%            objects with the specified name(s).
+%
+% Input:
+% glib :   input gds_library object
+% sname :  string with name of a structure or cell array
+%          of strings
+%
+% Output:
+% cas :    cell array of gds_structure objects
+
+% Initial version, Ulf Griesmann, February 2015
+
+    % check input arguments
+    if ~ischar(sname) && ~iscell(sname)
+        error('Argument ''sname'' must be string or cell array.');
+    end
+    if ischar(sname)
+        sname = {sname};
+    end
+    
+    % find all structure names from library
+    N = cellfun(@sname, glib.st, 'UniformOutput',0);
+
+    % find indices of structures
+    stri = find(ismember(N,sname) > 0);
+    if length(stri) < length(sname)
+        error('One or more structures not found in library.');
+    end
+    
+    % return structures
+    cas = glib.st(stri);
+    
+end
--- Basic/@gds_library/subtree.m
+++ Basic/@gds_library/subtree.m
@@ -1,37 +1,40 @@
-function [si] = subtree(glib, sname);
-%function [si] = subtree(glib, sname);
+function [cas] = subtree(glib, sname);
+%function [cas] = subtree(glib, sname);
 %
-% si :  indices of all structures in the library
-%       that make up a subtree of structures in a 
-%       gds_library object.
+% subtree: a method that returns a cell array of
+%             gds_structure objects that form a sub-tree 
+%             of structures in a library with a user
+%             specified structure as its top level.
 %
+% Input:
 % glib :   input gds_library object
 % sname :  name of the subtree top structure
-% si :     structure indices 
+%
+% Output:
+% cas :    cell array of gds_structure objects 
 
-% Initial version, Ulf Griesmann, December 2011
+% Initial version, Ulf Griesmann, February 2015
 
-% indices of structures in subtree
-subtree_ind = [];
+    % indices of structures in subtree
+    subtree_ind = [];
 
-% compute adjacency matrix of input library
-[A,N] = adjmatrix(glib.st);
+    % compute adjacency matrix of input library
+    [A,N] = adjmatrix(glib.st);
 
-% find index of structure 'sname'
-stri = find(ismember(N,{sname}) > 0);
-if isempty(stri)
-   error(sprintf('Structure >>> %s <<< not found in library.', sname));
-end
+    % find index of structure 'sname'
+    stri = find(ismember(N,{sname}) > 0);
+    if isempty(stri)
+        error(sprintf('Structure >>> %s <<< not found in library.', sname));
+    end
 
-% find the indices of all children
-find_children(A, stri);
-si = sort(unique(subtree_ind));
+    % find the indices of all children
+    find_children(A, stri);
+    cas = glib.st(sort(unique(subtree_ind)));
 
-   function find_children(A, pai);
+    function find_children(A, pai);
       %
-      % Recursively find all children of node si.
+      % Recursively find all children of node pai.
       %
-
       for p = pai
   
          % add index to array

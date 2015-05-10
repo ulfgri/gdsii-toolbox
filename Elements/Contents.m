70ffb9e19fbcc4f574f67d5ae7caad0f44c61570
Ulf GRIESMANN <ulfgri@gmail.com>
2015-05-10 12:01:39 -0400

    10may2015 - new function 'Elements/gdsii_pathtext.m' which renders a string   using path elements. - renamed existing 'gdsii_ptext.m' --> 'gdsii_boundarytext.m'

--- Elements/Contents.m
+++ Elements/Contents.m
@@ -1,9 +1,10 @@
 % Functions that Return gds_element Objects 
 % =========================================
 %
-% gdsii_ptext -  renders a text string with boundary elements.
-% gdsii_arc   -  creates arcs, circles, and circle segments.
+% gdsii_boundarytext -  renders a text string with boundary elements.
+% gdsii_pathtext     -  renders a text string with path elements.
+% gdsii_arc          -  creates arcs, circles, and circle segments.
 %
-% Ulf Griesmann, NIST, 2008 - 2013
+% Ulf Griesmann, NIST, 2008 - 2015
 % -------------------------------------------------------
 
---
+++ Elements/private/poly_rotz.m
@@ -0,0 +1,15 @@
+function [mpos] = poly_rotz(mp, ang)
+%function [mpos] = poly_rotz(mp, ang)
+%
+% poly_rotz : transformation of points by rotation around the z-axis
+%
+% mpos : points to be transformed; one per row
+% ang  : angle around z
+
+% Taken from the NIST Optics Toolbox
+
+    mpos = zeros(size(mp));
+    mpos(:,1) = cos(ang)*mp(:,1) - sin(ang)*mp(:,2);
+    mpos(:,2) = sin(ang)*mp(:,1) + cos(ang)*mp(:,2);
+  
+end

37617a5a60ce15560d463ab9aa560a1629921b1e
Ulf GRIESMANN <ulfgri@gmail.com>
2015-07-20 19:50:49 -0400

    20jul2015 - cosmetic improvements to printout from '@gds_library/treeview.m'   method.

--- Basic/@gds_library/treeview.m
+++ Basic/@gds_library/treeview.m
@@ -10,45 +10,47 @@ function treeview(glib);
 
 % initial version, Ulf Griesmann, NIST, December 2011
 
-% calculate the adjacency matrix of the structure tree
-[A,N] = adjmatrix(glib.st);
+    % calculate the adjacency matrix of the structure tree
+    [A,N] = adjmatrix(glib.st);
 
-% find top level structure(s) - they have no parents
-pai = find(sum(A)==0);  % top parent index (or indices)
+    % find top level structure(s) - they have no parents
+    pai = find(sum(A)==0);  % top parent index (or indices)
+    
+    % display names beginning with the top
+    fprintf('\n');
+    display_tree(A, N, pai, 0);
+    fprintf('\n');
 
-% display names beginning with the top
-display_tree(A, N, pai, 0);
-
-return
+end
 
 function display_tree(A, N, pai, indent);
 %
 % Function, called recursively, to display the structure 
 % tree described by an adjacency matrix.
 %
-for p = pai
+    for p = pai
 
-   % print parent name
-   if indent
-      blank(1:indent) = ' ';
-      fprintf('%s%s\n', blank, N{p}); % parent
-   else
-      fprintf('%s\n', N{p}); % parent
-   end
+        % print parent name
+        if indent
+            blank(1:indent) = ' ';
+            fprintf('%s%s\n', blank, N{p}); % parent
+        else
+            fprintf('%s\n', N{p}); % parent
+        end
    
-   % find children
-   chi = find(A(p,:)>0);
+        % find children
+        chi = find(A(p,:)>0);
    
-   % next parent if there are no children
-   if isempty(chi)
-      continue
-   end
+        % next parent if there are no children
+        if isempty(chi)
+            continue
+        end
    
-   % otherwise print child generation
-   for c = chi
-      display_tree(A, N, c, indent+6);      
-   end
+        % otherwise print child generation
+        for c = chi
+            display_tree(A, N, c, indent+6);      
+        end
    
+    end
+
 end
-
-return

89a69286a0d371daabeee6a85982ab6ed5eb9c7e
Ulf GRIESMANN <ulfgri@gmail.com>
2015-07-21 21:34:43 -0400

    21jul2015 - renovate the method '@gds_library/topstruct.m'. The method now   displays the top level structure(s) when no output argument is   present. A cell array with structure names is returned otherwise.

--- Basic/@gds_library/topstruct.m
+++ Basic/@gds_library/topstruct.m
@@ -2,23 +2,29 @@ function [ts] = topstruct(glib);
 %function [ts] = topstruct(glib);
 %
 % topstruct :  return the name(s) of the top structure(s)
-%              in a gds_library object
+%              in a gds_library object or display the structure
+%              names if no output argument is present.
 %
 % glib : a gds_library object
-% ts :   name of the top structure or cell array with names if 
-%        the library has more than one top level structure
+% ts :   cell array with names of top level structures
 
 % Initial version, Ulf Griesmann, December 2011
 
-% calculate the adjacency matrix of the structure tree
-[A,N] = adjmatrix(glib.st);
+    % calculate the adjacency matrix of the structure tree
+    [A,N] = adjmatrix(glib.st);
 
-% find top level structure name(s)
-naml = N(find(sum(A)==0));
-if length(naml) == 1
-   ts = naml{1};
-else
-   ts = naml;
+    % find top level structure name(s)
+    tops = N(find(sum(A)==0));
+
+    % display or return the structure names
+    if nargout
+        ts = tops;
+    else
+        fprintf('\n');
+        for k = 1:length(tops)
+            fprintf('   %s\n', tops{k});
+        end
+        fprintf('\n');
+    end
+
 end
-
-return
--- Basic/funcs/topstruct.m
+++ Basic/funcs/topstruct.m
@@ -1,30 +1,32 @@
-function [ts] = topstruct(cas, bcell);
-%function [ts] = topstruct(cas, bcell);
+function [ts] = topstruct(cas)
+%function [ts] = topstruct(cas)
 %
 % topstruct :  return the name(s) of the top structure(s)
 %              in a cell array of gds_structure objects
+%              or display the top structures.
 %
 % cas :   a cell array of gds_structure objects
-% bcell : if == 1, always return a cell array, even when only
-%         one top level structure is found. Default is 0.
 % ts :    a string with the name of the top structure or cell 
 %         array with names if the library has more than one top 
 %         level structure
 
 % Initial version, Ulf Griesmann, December 2012
 
-    % check arguments
-    if nargin < 2, bcell = []; end
-
-    if isempty(bcell), bcell = 0; end
-
     % calculate the adjacency matrix of the structure tree
     [A,N] = adjmatrix(cas);
 
     % find top level structure name(s)
-    ts = N(find(sum(A)==0));
-    if length(ts) == 1 && ~bcell
-        ts = ts{1}; 
+    tops = N(find(sum(A)==0));
+
+    % display or return the structure names
+    if nargout
+        ts = tops;
+    else
+        fprintf('\n');
+        for k = 1:length(tops)
+            fprintf('   %s\n', tops{k});
+        end
+        fprintf('\n');
     end
 
 end

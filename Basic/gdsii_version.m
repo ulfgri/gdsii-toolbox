dd3d45dae98a2eab95c898eaacdfda0a9f1e2097
Ulf GRIESMANN <ulfgri@gmail.com>
2015-11-08 10:05:04 -0500

    08nov2015 - version 126

--- Basic/gdsii_version.m
+++ Basic/gdsii_version.m
@@ -16,8 +16,8 @@ function [tb_ver, sw_ver] = gdsii_version;
 %
 
 % toolbox version and version date
-tb_version = '125';
-tb_date = '2015-June-15';
+tb_version = '126';
+tb_date = '2015-Nov-08';
 ltb_ver = [tb_version, '  (', tb_date, ')'];
 if exist('OCTAVE_VERSION')
     interpreter = 'Octave';

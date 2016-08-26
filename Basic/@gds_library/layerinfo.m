291256a81996c3108585dff65c952491653ddd10
Ulf GRIESMANN <ulfgri@gmail.com>
2016-08-25 21:34:20 -0400

    25aug2016 - make 'layerinfo.m' a method of the gds_library class.

--- Basic/@gds_library/layerinfo.m
+++ Basic/@gds_library/layerinfo.m
@@ -17,29 +17,24 @@ function [S,L] = layerinfo(glib)
 
 % initial version, Ulf Griesmann, NIST, November 16, 2012
 
-    % check argument
-    if ~isa(glib, 'gds_library')
-        error('layerinfo :  argument must be a gds_library object.');
-    end
-    
     % initialize variables for accounting
     numl = 256; % max number of layers
-    S = repmat(struct('boundary',0,'path',0,'box',0,'node',0,'text',0),1,numl);
-    L = zeros(1,numl);
+    Si = repmat(struct('boundary',0,'path',0,'box',0,'node',0,'text',0),1,numl);
+    Li = zeros(1,numl);
     
     % iterate over all structures
-    for k=1:numst(glib)
+    for k=1:numel(glib.st)
         
         % structure
-        st = glib(k);
+        st = glib.st{k};
         
         % iterate over all elements
         for m=1:numel(st)
             el = st(m);
             if ~is_ref(el)            % sref and aref have no layer information
                 numl = el.layer + 1;  % gds layer numbers start with 0
-                L(numl) = L(numl) + 1; 
-                S(numl).(etype(el)) = S(numl).(etype(el)) + 1;
+                Li(numl) = Li(numl) + 1; 
+                Si(numl).(etype(el)) = Si(numl).(etype(el)) + 1;
             end
         end
     end
@@ -47,29 +42,34 @@ function [S,L] = layerinfo(glib)
     %display
     if ~nargout
         fprintf('\n');
-        for k = find(L)
+        for k = find(Li)
             fprintf('   L %-3d ->  ', k-1); % layers start with 0
-            if S(k).boundary
-                fprintf('%8d Bnd ', S(k).boundary);
+            if Si(k).boundary
+                fprintf('%8d Bnd ', Si(k).boundary);
             end
-            if S(k).path
-                fprintf('%8d Pth ', S(k).path);
+            if Si(k).path
+                fprintf('%8d Pth ', Si(k).path);
             end
-            if S(k).box
-                fprintf('%8d Box ', S(k).box);
+            if Si(k).box
+                fprintf('%8d Box ', Si(k).box);
             end
-            if S(k).node
-                fprintf('%8d Nde ', S(k).node);
+            if Si(k).node
+                fprintf('%8d Nde ', Si(k).node);
             end
-            if S(k).text
-                fprintf('%8d Txt ', S(k).text);
+            if Si(k).text
+                fprintf('%8d Txt ', Si(k).text);
             end 
             fprintf('\n');
         end
         fprintf('\n');
-    end
+    else
     
-    % return layer numbers with elements
-    L = find(L) - 1;
+        % return layer numbers with elements
+        L = find(Li) - 1;
+        if nargout > 1
+            S = Si;
+        end
+        
+    end
     
 end

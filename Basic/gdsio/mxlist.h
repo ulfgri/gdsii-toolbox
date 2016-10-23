57b3a642402bdb393ea9ae0f5147786d95cfdac8
Ulf GRIESMANN <ulfgri@gmail.com>
2016-10-22 20:48:57 -0400

    - mxlist.h: conform to C99 (default for GCC 5)

--- Basic/gdsio/mxlist.h
+++ Basic/gdsio/mxlist.h
@@ -15,12 +15,6 @@
 #ifndef _mxlist_h
 #define _mxlist_h
 
-#ifdef __GNUC__
-#define INLINE  __inline__
-#else
-#define INLINE
-#endif
- 
 
 /*
  * ANSI C compiler happyfiers
@@ -45,35 +39,35 @@ typedef char * tList;
 /*
  * Create a List. Returns -1 if not successful, 0 otherwise
  */
-extern INLINE int 
+int 
 create_list( tList *list );
 
 
 /*
  * Create a List and return it. Return NULL if not successful.
  */
-extern INLINE tList
+tList
 new_list();
 
 
 /*
  * erase all entries in the list, leaving an empty list
  */
-extern INLINE int
+int
 erase_list_entries( tList list );
 
 
 /* 
  * Delete a list from memory (List must be empty)
  */
-extern INLINE int 
+int 
 delete_list( tList *list );
 
 
 /*
  * Return number of elements in the list (list must exist already)
  */
-extern INLINE int 
+int 
 list_entries( tList list );
 
 
@@ -81,7 +75,7 @@ list_entries( tList list );
  * advance the list pointer to the next entry if not at end of list
  * wraps round to first element at the end of the list
  */
-extern INLINE void
+void
 list_next( tList list );
 
 
@@ -89,42 +83,42 @@ list_next( tList list );
  * advance the list pointer to the previous entry if not at beginning of list
  * wraps round to last element at the end of the list
  */
-extern INLINE void
+void
 list_prev( tList list );
 
 
 /*
  * Reset the list pointer to the beginning of the list
  */
-extern INLINE void 
+void 
 list_head( tList list );
 
 
 /*
  * set the list pointer to the last entry in the list
  */
-extern INLINE void
+void
 list_tail( tList list);
 
 
 /*
  * returns 1 if the list pointer is set to the first element in the list, 0 otherwise
  */
-extern INLINE int
+int
 list_at_head( tList list );
 
 
 /*
  * returns 1 if the list pointer is set to the last element in the list, 0 otherwise
  */
-extern INLINE int
+int
 list_at_tail( tList list);
 
 
 /*
  * stores the current list pointer
  */
-extern INLINE void
+void
 list_store_current(tList list);
 
 
@@ -133,7 +127,7 @@ list_store_current(tList list);
  * (This should only be used if the list has not been changed since
  * the last call to 'list_store_current'.
  */
-extern INLINE void
+void
 list_recall_current(tList list);
 
 
@@ -142,7 +136,7 @@ list_recall_current(tList list);
  * The new entry is inserted either BEFORE or AFTER the current pointer.
  * The BEFORE option enables the creation of stacks (=FIFO lists).
  */
-extern INLINE int 
+int 
 list_insert( tList list, void *new_entry, int location );
 
 
@@ -152,7 +146,7 @@ list_insert( tList list, void *new_entry, int location );
  * allocate memory for the list entry. This is essentially a 
  * convenience function;
  */
-extern INLINE int
+int
 list_insert_object( tList list, void *new_object, int obj_size, int location );
 
 
@@ -163,14 +157,14 @@ list_insert_object( tList list, void *new_object, int obj_size, int location );
  * if the first argument is smaller than the second; comp_proc > 0 if
  * the first argument is larger than the second.
  */
-extern INLINE int 
+int 
 sorted_list_insert(tList list, void *new_entry, int (*comp_proc)(void *, void *));
 
 
 /*
  * a convenience version of the previous function
  */
-extern INLINE int 
+int 
 sorted_list_insert_object(tList list, void *new_object, int obj_size, int (*comp_proc)(void *, void *));
 
 
@@ -178,7 +172,7 @@ sorted_list_insert_object(tList list, void *new_object, int obj_size, int (*comp
  * Return a pointer to the current, increment list pointer
  * At the end of the list the list pointer wraps round to the beginning.
  */
-extern INLINE void *
+void *
 get_current_entry( tList list, void **ptr_to_entry );
 
 
@@ -186,7 +180,7 @@ get_current_entry( tList list, void **ptr_to_entry );
  * retrieve the current object, increment the list pointer.
  * This too is a convenience function.
  */
-extern INLINE void *
+void *
 get_current_object( tList list, void *object, int obj_size );
 
 
@@ -194,14 +188,14 @@ get_current_object( tList list, void *object, int obj_size );
  * removes the current list entry. Current pointer is set to prev entry.
  * DOES ALSO DEALLOCATE DATA
  */
-extern INLINE void
+void
 remove_current_entry( tList list );
 
 
 /*
  * remove the tail entry of the list, discard content
  */
-extern INLINE void
+void
 chop_list_tail( tList list );
 
 
@@ -209,7 +203,7 @@ chop_list_tail( tList list );
  * Apply a function to all members of the list.
  * Each list entry is passed to the function 'scan_proc'.
  */
-extern INLINE void
+void
 scan_list( tList list,  void (*scan_proc)(void *) );
 
 
@@ -219,14 +213,14 @@ scan_list( tList list,  void (*scan_proc)(void *) );
  * NULL otherwise. search_proc MUST return either 0 or 1.
  * The list pointer is positioned behind the found entry !
  */
-extern INLINE void *
+void *
 search_list( tList list, int (*search_proc)(void *) );
 
 
 /*
  * a convenience version of the previous function
  */
-extern INLINE void *
+void *
 search_list_object(tList list, void *object, int obj_size, int (*search_proc)(void *) );
 
 

1f541903799dd439c77d74a653c1505900b428f2
Ulf GRIESMANN <ulfgri@gmail.com>
2019-11-28 11:39:16 -0500

    - removed 32-character restriction on the length of structure names - structre names are no longer padded with ' ' to make their length even

--- Basic/gdsio/gds_beginstruct.c
+++ Basic/gdsio/gds_beginstruct.c
@@ -13,12 +13,11 @@
  * 
  */
 
+#include <string.h>
+
 #include "gdsio.h"
 #include "mex.h"
 #include "mexfuncs.h"
-
-#define NLEN   48
-
 
 /*-----------------------------------------------------------------*/
 
@@ -26,11 +25,11 @@ void
 mexFunction(int nlhs, mxArray *plhs[], 
 	    int nrhs, const mxArray *prhs[])
 {
-   FILE *fob;          /* file object pointer */
-   date_t cdate;       /* creation date */
-   date_t mdate;       /* modification date */
-   char sname[NLEN];   /* structure name */
-   int snlen;          /* structure name length */
+   FILE *fob;     /* file object pointer */
+   date_t cdate;  /* creation date */
+   date_t mdate;  /* modification date */
+   char *sname;   /* structure name */
+   int snlen;     /* structure name length */
 
    /* check argument number */
    if (nrhs != 3) {
@@ -55,17 +54,12 @@ mexFunction(int nlhs, mxArray *plhs[],
       mexErrMsgTxt("failed to write BGNSTR record (mdate).");
    
    /* STRNAME record */
-   snlen = mxGetNumberOfElements(prhs[1]);  /* string length */
-   if (snlen > 32) {
-      mexPrintf("\nStructure name %s exceeds 32 characters\n\n", sname);
-      mexErrMsgTxt("structure name too long.");     
-   }
-   mxGetString(prhs[1], sname, NLEN-2);
-   if (snlen % 2)          /* string length is odd */
-      snlen += 1;
-   if ( write_record_hdr(fob, STRNAME, snlen) )
+   sname = mxArrayToString(prhs[1]);  /* copy structure name */
+   if ( !sname )
+      mexErrMsgTxt("failed to allocate buffer for structure name.");     
+   if ( write_record_hdr(fob, STRNAME, strlen(sname)) )
       mexErrMsgTxt("failed to write STRNAME record header.");
-   if ( write_string(fob, sname, snlen) )
+   if ( write_string(fob, sname, strlen(sname)) )
       mexErrMsgTxt("failed to write STRNAME record (sname).");
 }
 
